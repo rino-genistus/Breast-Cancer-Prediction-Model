d9cd5822abd207b0f545ac1974a708abcb471207
Rino Genistus Ruban <rino.genistus@gmail.com>
2026-01-03 19:44:31 -0800

    Worked on logistic regression model and got something working, basic

--- Model.py
+++ Model.py
@@ -10,14 +10,23 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 
 data = pd.read_csv('C:/Users/rinog/Downloads/archive/data.csv')
-print(f"Data.head(): {data.head()}")
-print(f"Data.info(): {data.info()}")
-print(f"Data.describe(): {data.describe()}")
+#print(f"Data.head(): {data.head()}")
+#print(f"Data.info(): {data.info()}")
+#print(f"Data.describe(): {data.describe()}")
+print(f"Data fractal_dimension_worst: {data['fractal_dimension_worst']}")
 
-X = data.drop(columns=['id', 'diagnosis'])
+X = data.drop(columns=['id', 'diagnosis', 'Unnamed: 32'])
+print(f"Datatypes: {X.dtypes}")
 Y = data['diagnosis'].map({'M':1, "B":0})
 
 X_train,X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state=55, stratify=Y)
+
+LReg = LogisticRegression(random_state=55)
+LReg.fit(X_train, Y_train)
+
+y_pred = LReg.predict(X_test)
+print(y_pred)
